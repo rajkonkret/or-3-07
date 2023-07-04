c16cdb5074bdd7e228747be96ce9379274b04546
CSComarch <rajkonkret660@gmail.com>
2023-07-04 10:53:31 +0200

    add wyjatki2

---
+++ wyjatki2-case.py
@@ -0,0 +1,30 @@
+while True:
+    try:
+        z = input("Podaj działanie + - * /")
+        a = int(input("Podaj pierwsza liczbe"))
+        b = int(input("Podaj druga liczbe"))
+
+        match z:
+            case "+":
+                print(f"Wynik dodawania {a} + {b} = {a + b}")
+            case "-":
+                print(f"Wynik odejmowania {a} - {b} = {a - b}")
+
+            case "*":
+                print(f"Wynik mnożenia {a} * {b} = {a * b}")
+            case "/":
+                print(f"Wynik dzielenia {a} / {b} = {a / b}")
+            case _:
+                break
+    except ZeroDivisionError:
+        print("Dzielenie przez zero")
+    except TypeError:
+        print("Bład typu")
+    except ValueError:
+        print("Błąd wartości")
+    except Exception as e:
+        print("Bład", e)
+    else:
+        print("Nie ma błędu")
+    finally:
+        print("Zawsze")
