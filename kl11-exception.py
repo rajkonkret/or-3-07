28fdbd04e7784f4b953ff1d276941da4b7ce8549
CSComarch <rajkonkret660@gmail.com>
2023-07-06 10:57:03 +0200

    add kl11

---
+++ kl11-exception.py
@@ -0,0 +1,11 @@
+class MyException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+try:
+    raise MyException("wystąpił wyjątek")  # rzucenie wyjątku, MyException: wystąpił wyjątek
+except MyException:
+    print("Wystąpił wyjątek MyException")
+except Exception as e:
+    print("Bład", e)
