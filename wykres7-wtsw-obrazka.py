c84acc781df794317e16f4fd9c2dc8f2ef348315
CSComarch <rajkonkret660@gmail.com>
2023-07-07 10:31:29 +0200

    add wykres7

---
+++ wykres7-wtsw-obrazka.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+
+img = mpimg.imread('user_photo.jpg')
+
+plt.imshow(img)
+plt.axis('off')
+plt.show()
+
+# wczytanie obrazka z apomocą PIL
+image = Image.open('user_photo.jpg')
+
+image.show()
